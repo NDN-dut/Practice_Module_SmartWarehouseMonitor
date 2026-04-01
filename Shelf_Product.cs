20177ced4c838b72f9406628079185a46c898bc8
NDNgon <102240325@sv1.dut.udn.vn>
2026-04-01 20:42:41 +0700

    first commit

---
+++ Shelf_Product.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Namespace
+{
+    class Shelf_Product
+    {
+        public int ShelfId { get; set; }
+        public int ProductId { get; set; }
+        public int ProductQuantity { get; set; }
+    }
+}
---
+++ WarehouseManager.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartShelfMonitor
+{
+    class WarehouseManager
+    {
+        private static WarehouseManager _Instance;
+        public static WarehouseManager Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                    _Instance = new WarehouseManager();
+                return _Instance;
+            }
+            private set { }
+        }
+    }
+}
